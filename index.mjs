ae93f67451ba7928d06e99ed0ce846e78f40021e
Kingdavid <kingigbayilola@gmail.com>
2024-06-30 23:37:22 +0100

    check

--- index.mjs
+++ index.mjs
@@ -15,26 +15,31 @@ app.get("/", (req, res) => {
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
+  // const clientIp = req.ip.replace("::ffff:", "");
   const geo = geoip.lookup(req.clientIp);
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${geo.ll[0]}&longitude=${geo.ll[1]}&hourly=temperature_2m`;
+  // const url = `https://api.open-meteo.com/v1/forecast?latitude=${geo.ll[0]}&longitude=${geo.ll[1]}&hourly=temperature_2m`;
 
-  if(!visitor_name) {
-    return res.status(400).json({
-      status: 400,
-      message: "Please provide visitor name",
-    });
-  }
-
+  // if(!visitor_name) {
+  //   return res.status(400).json({
+  //     status: 400,
+  //     message: "Please provide visitor name",
+  //   });
+  // }
+  const ip = req.clientIp;
   try {
-    const result = await axios.get(url);
-    const {hourly, hourly_units } = result.data;
+    // const result = await axios.get(url);
+    // const {hourly, hourly_units } = result.data;
 
-    const temp = `${hourly.temperature_2m[0]} ${hourly_units.temperature_2m}`;
-    return res.status(200).json({
-      client_ip: clientIp,
-      location: geo.city,
-      greeting: `Hello ${visitor_name}, the temperature is ${temp} degrees Celsius in ${geo.city}`,
-    });
+    // const temp = `${hourly.temperature_2m[0]} ${hourly_units.temperature_2m}`;
+    // return res.status(200).json({
+    //   client_ip: clientIp,
+    //   location: geo.city,
+    //   greeting: `Hello ${visitor_name}, the temperature is ${temp} degrees Celsius in ${geo.city}`,
+    // });
+    return res.json({
+      ip,
+      geo
+    })
   } catch (error) {
     const err = new Error(error);
     err.statusCode = 500;
