fb0fe27ea378ef0dd651bf3969107b9c56ca50a6
Kingdavid <kingigbayilola@gmail.com>
2024-06-30 23:31:37 +0100

    test

--- index.mjs
+++ index.mjs
@@ -3,39 +3,42 @@ dotenv.config({ path: `${process.cwd()}/.env` });
 import express from "express";
 import geoip from "geoip-lite";
 import axios from "axios";
-
+import requestIp from "request-ip";
 const app = express();
 
+// middlewareIp 
+app.use(requestIp.mw());
 
+app.use(reques)
 app.get("/", (req, res) => {
   res.send("HNG stage one task");
 });
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
-  const clientIp = req.ip.replace("::ffff:", "");
-  const geo = geoip.lookup(req.ip);
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${geo.ll[0]}&longitude=${geo.ll[1]}&hourly=temperature_2m`;
+  // const clientIp = req.ip.replace("::ffff:", "");
+  // const geo = geoip.lookup(req.ip);
+  // const url = `https://api.open-meteo.com/v1/forecast?latitude=${geo.ll[0]}&longitude=${geo.ll[1]}&hourly=temperature_2m`;
 
-  if(!visitor_name) {
-    return res.status(400).json({
-      status: 400,
-      message: "Please provide visitor name",
-    });
-  }
+  // if(!visitor_name) {
+  //   return res.status(400).json({
+  //     status: 400,
+  //     message: "Please provide visitor name",
+  //   });
+  // }
+  const ip = req.clientIp;
   try {
-    const result = await axios.get(url);
-    const {hourly, hourly_units } = result.data;
+    // const result = await axios.get(url);
+    // const {hourly, hourly_units } = result.data;
 
-    const temp = `${hourly.temperature_2m[0]} ${hourly_units.temperature_2m}`;
+    // const temp = `${hourly.temperature_2m[0]} ${hourly_units.temperature_2m}`;
     // return res.status(200).json({
     //   client_ip: clientIp,
     //   location: geo.city,
     //   greeting: `Hello ${visitor_name}, the temperature is ${temp} degrees Celsius in ${geo.city}`,
     // });
     return res.json({
-      ip: req.ip,
-      geo
+      ip
     })
   } catch (error) {
     const err = new Error(error);
